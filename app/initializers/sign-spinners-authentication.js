63d149d8421fddbe305bccaa0ab2e3c0dd792d4b
Ryan Berckmans <ryanberckmans@gmail.com>
2014-10-02 08:14:25 -0400

    sign-spinners-authentication.js, refactor, now uses a singleton FirebaseSimpleLogin client

--- app/initializers/sign-spinners-authentication.js
+++ app/initializers/sign-spinners-authentication.js
@@ -44,7 +44,6 @@ var SignSpinnersAuthentication =  Ember.Object.extend({
       Ember.Logger.error('login expected _currentUser to be null');
       return Ember.RSVP.reject('attempted to login when authenticated');
     }
-
     if (email === undefined) {
       return this._loginActiveSession();
     }
@@ -61,24 +60,9 @@ var SignSpinnersAuthentication =  Ember.Object.extend({
       Ember.Logger.error('logout expected _currentUser to exist');
       return Ember.RSVP.reject('attempted to logout when not authenticated');
     }
-
-    var self = this;
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      var authClient = new window.FirebaseSimpleLogin(dbRef, function(error, user) {
-        Ember.run(function() {
-          if (error) {
-            reject(error);
-          }
-
-          if (!user) {
-            Ember.Logger.info('SignSpinnersAuthentication: logged out');
-            self.set('_currentUser', null);
-            resolve(null);
-          }
-        });
-      });
-      authClient.logout();
-    });
+    var promise = this._getFirebaseSimpleLoginPromise();
+    this._getFirebaseSimpleLoginClient().logout();
+    return promise;
   },
 
   // Public.
@@ -91,40 +75,22 @@ var SignSpinnersAuthentication =  Ember.Object.extend({
       Ember.Logger.error('createNewUser expected _currentUser to be null');
       return Ember.RSVP.reject('attempted to create new user when authenticated');
     }
-
-    var self = this;
-    var promise = new Ember.RSVP.Promise(function(resolve, reject) {
-        var authClient = new window.FirebaseSimpleLogin(dbRef, function(error, user) {
-          Ember.run(function() {
-            if (error) {
-              reject(error);
-            }
-
-            // ie the created user was successfully logged in
-            if (user) {
-              // ****
-              // **** TBD - Must set authentication data in firebase. Be careful not to set _currentUser until the authentication data exists in Firebase.
-              // ****       Observers on _currentUser will expect the logged in user to be fully-formed, ie authentication/$uid exists
-              // ****
-              self.set('_currentUser', user);
-              resolve(user);
-            }
-          });
-        });
-
-        authClient.createUser(email, password, function(error, user) {
-          Ember.run(function() {
-            if (error) {
-              reject(error);
-            }
-
-            if (user) {
-              authClient.login('password', {email: email, password: password});
-            }
-          });
-        });
+    var _this = this;
+    var promise = this._getFirebaseSimpleLoginPromise();
+    this._getFirebaseSimpleLoginClient().createUser(email, password, function(error, user) {
+      Ember.run(function() {
+        if (error) {
+          _this.get('_firebaseSimpleLoginPromiseReject')(error);
+        }
+        if (user) {
+          // ****       User is created, but firebase/authentication/uid doesn't exist, nor authentication/owned_{businesses,spinners}/{business,spinner}_id              
+          // **** TBD - Must set authentication data in firebase. Be careful not to set _currentUser until the authentication data exists in Firebase.
+          // ****       Observers on _currentUser will expect the logged in user to be fully-formed, ie authentication/$uid exists
+          // ****
+          _this._getFirebaseSimpleLoginClient().login('password', {email: email, password: password});
+        }
+      });
     });
-
     return promise;
   },
 
@@ -135,73 +101,102 @@ var SignSpinnersAuthentication =  Ember.Object.extend({
   _currentUser: null, // We define "Is the user currently authenticated?" as _currentUser !== null.
   _loginOnInitPromise: null,
 
+  _firebaseSimpleLoginClient: null,
+  _firebaseSimpleLoginPromise: null, // Promise used inside FirebaseSimpleLogin callback. Create new for each operation, unless the old promise still exists.
+  _firebaseSimpleLoginPromiseResolve: null, // resolve() handler for _firebaseSimpleLoginPromise
+  _firebaseSimpleLoginPromiseReject: null,  // reject()  handler for _firebaseSimpleLoginPromise
+
+  // Sets up a new Promise for use inside FirebaseSimpleLogin callback, including a finally() handler to clear this Promise when it has been settled
+  // The new promise is set as _firebaseSimpleLoginPromise
+  // @return {Promise} for convenience, returns the new Promise, or existing Promise if it hasn't been settled yet
+  _getFirebaseSimpleLoginPromise: function() {
+    var _this = this;
+    var firebaseSimpleLoginPromise = this.get('_firebaseSimpleLoginPromise');
+    if (firebaseSimpleLoginPromise === null) {
+      firebaseSimpleLoginPromise = new Ember.RSVP.Promise(function(resolve, reject) {
+        if (_this.get('_firebaseSimpleLoginPromiseResolve') === null) {
+          _this.set('_firebaseSimpleLoginPromiseResolve', resolve);
+        } else {
+          throw new Error('expected _firebaseSimpleLoginPromiseResolve to be null since _firebaseSimpleLoginPromise is null');
+        }
+        if (_this.get('_firebaseSimpleLoginPromiseReject') === null) {
+          _this.set('_firebaseSimpleLoginPromiseReject', reject);
+        } else {
+          throw new Error('expected _firebaseSimpleLoginPromiseReject to be null since _firebaseSimpleLoginPromise is null');
+        }
+      });
+      firebaseSimpleLoginPromise.finally(function() {
+        // Remove this Promise from this Object when it has been settled - it should be used exactly once in the FirebaseSimpleLogin callback
+        // When this finally() handler is invoked, we expect that _firebaseSimpleLoginPromise is still set to this new Promise
+        var firebaseSimpleLoginPromise2 = _this.get('_firebaseSimpleLoginPromise');
+        if (firebaseSimpleLoginPromise2 !== firebaseSimpleLoginPromise) {
+          throw new Error("_firebaseSimpleLoginPromise appears to have been set to a new Promise before the old Promise's finally() handler was invoked");
+        }
+        _this.set('_firebaseSimpleLoginPromise', null);
+        _this.set('_firebaseSimpleLoginPromiseResolve', null);
+        _this.set('_firebaseSimpleLoginPromiseReject', null);
+        Ember.Logger.debug('cleared _firebaseSimpleLoginPromise');
+      });
+      Ember.Logger.debug('set new _firebaseSimpleLoginPromise');
+      this.set('_firebaseSimpleLoginPromise', firebaseSimpleLoginPromise);
+    } else {
+      throw new Error("FirebaseSimpleLogin operation already in progress: _firebaseSimpleLoginPromise not null");
+    }
+    return firebaseSimpleLoginPromise;
+  },
+
+  // Must be preceeded with a call to _getFirebaseSimpleLoginPromise(), which sets up a new Promise for use in any FirebaseSimpleLogin operation
+  // @return {FirebaseSimpleLogin} singleton client, creating it if necessary
+  _getFirebaseSimpleLoginClient: function() {
+    var _this = this;
+    var firebaseSimpleLoginClient = this.get('_firebaseSimpleLoginClient');
+    if (firebaseSimpleLoginClient === null ) {
+      Ember.Logger.debug('creating new FirebaseSimpleLogin client');
+      firebaseSimpleLoginClient = new window.FirebaseSimpleLogin(dbRef, function(error, user) {
+        Ember.run(function() {
+          if (error && error.code === 'INVALID_USER') {
+            Ember.Logger.warn('SignSpinnersAuthentication: received INVALID_USER');
+            _this.get('_firebaseSimpleLoginPromiseResolve')(false);
+          } else if (error) {
+            _this.get('_firebaseSimpleLoginPromiseReject')(error);
+          } else if (!error && !user) {
+            Ember.Logger.info('SignSpinnersAuthentication: logged out or found no existing session');
+            _this.set('_currentUser', null);
+            _this.get('_firebaseSimpleLoginPromiseResolve')(false);
+          } else {
+            if (!user) {
+              throw new Error("expected !null user because boolean algrebra");
+            }
+            Ember.Logger.info('SignSpinnersAuthentication: re-established existing session');
+            _this.set('_currentUser', user);
+            _this.get('_firebaseSimpleLoginPromiseResolve')(true);
+          }
+        });
+      });
+      this.set('_firebaseSimpleLoginClient', firebaseSimpleLoginClient);
+    }
+    return firebaseSimpleLoginClient;
+  },
+
   // Attempts to login a new session with the passed email and password, and sets _currentUser if the login is successful
   // @return {Promise} resolves true if the login is successful, false otherwise
   _loginWithCredentials: function(email, password) {
     Ember.Logger.debug("SignSpinnersAuthentication: starting _loginWithCredentials");
-    var self = this;
-    // Setup a promise that creates the FirebaseSimpleLogin and resolves
-    var promise = new Ember.RSVP.Promise(function(resolve, reject) {
-      var authClient = new window.FirebaseSimpleLogin(dbRef, function(error, user) {
-        //First Time this fires error and user should be null. If connection successful
-        //Second Time will be due to login. In that case we should have user or error
-        Ember.run(function() {
-          // TBD handle every error code - https://www.firebase.com/docs/web/guide/user-auth.html#section-full-error
-          if (error && error.code === 'INVALID_USER') {
-            resolve(false);
-          } else if (error) {
-            reject(error);
-          }
-
-          if (user) {
-            Ember.Logger.info('SignSpinnersAuthentication: logged in with email+password');
-            self.set('_currentUser', user);
-            resolve(true);
-          }
-        });
-      });
-      authClient.login('password', {
+    var promise = this._getFirebaseSimpleLoginPromise();
+    this._getFirebaseSimpleLoginClient().login('password', {
             email: email,
-            password: password
-      });
-    });
-
+            password: password});
     return promise;
   },
 
-  /* jshint ignore:start */
   // Attempts to login in an active session, and sets _currentUser if the login is successful
   // @return {Promise} resolves true if the login is successful, false otherwise
   _loginActiveSession: function() {
     Ember.Logger.debug("SignSpinnersAuthentication: starting _loginActiveSession");
-    var self = this;
-    // Setup a promise that creates the FirebaseSimpleLogin and resolves
-    var promise = new Ember.RSVP.Promise(function(resolve, reject) {
-      
-      var authClient = new window.FirebaseSimpleLogin(dbRef, function(error, user) {
-        // This callback should fire just once if no error or user than not logged in
-        Ember.run(function() {
-          if (!error && !user) {
-            Ember.Logger.info('SignSpinnersAuthentication: found no existing session');
-            resolve(false);
-          }
-
-          if (error) {
-            reject(error);
-          }
-
-          if (user) {
-            Ember.Logger.info('SignSpinnersAuthentication: re-established existing session');
-            self.set('_currentUser', user);
-            resolve(true);
-          }
-        });
-      });
-    });
-
+    var promise = this._getFirebaseSimpleLoginPromise();
+    this._getFirebaseSimpleLoginClient(); // initializing the client (if it hasn't already been initialized) is sufficient to login an active session
     return promise;
   }
-  /* jshint ignore:end */
 });
 
 var SignSpinnersAuthenticationInitializer = {
